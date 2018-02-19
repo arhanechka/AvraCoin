6860077d44a1cfd94e6f0510ae5da3a5bd1869b1
Your Name <arhanechka@gmail.com>
2018-02-19 10:13:36 +0200

    realisation of buying tokens

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,158 +1,173 @@
+// Import the page's CSS. Webpack will know what to do with it.
+import '../stylesheets/app.css'
 // Import libraries we need.
-// noinspection JSAnnotator
-import { default as Web3 } from 'web3';
-
-// noinspection JSAnnotator
+import { default as Web3 } from 'web3'
 import { default as contract } from 'truffle-contract'
-
 // Import our contract artifacts and turn them into usable abstractions.
-// noinspection JSAnnotator
-import GustavoCoinCrowdsale_artifacts from '../../build/contracts/GustavoCoinCrowdsale.json'
-// noinspection JSAnnotator
-import GustavoCoin_artifacts from '../../build/contracts/GustavoCoin.json'
-// AvraToken is our usable abstraction, which we'll use through the code below.
-var GustavoCoinCrowdsale = contract(GustavoCoinCrowdsale_artifacts);
-var GustavoCoin = contract(GustavoCoin_artifacts);
-
+import Avracoincrowdsale_artifacts from '../../build/contracts/Crowdsale.json'
+import Avracoin_artifacts from '../../build/contracts/AvraToken.json'
+// MetaCoin is our usable abstraction, which we'll use through the code below.
+var AvraCoinCrowdsale = contract(Avracoincrowdsale_artifacts)
+var AvraCoin = contract(Avracoin_artifacts)
 // The following code is simple to show off interacting with your contracts.
 // As your needs grow you will likely need to change its form and structure.
 // For application bootstrapping, check out window.addEventListener below.
-var accounts;
-var account;
+var accounts
+var account
+var account1
 
-App = {
-  web3Provider: null,
-  contracts: {},
+window.App = {
+  start: function () {
+    var self = this
 
+    // Bootstrap the MetaCoin abstraction for Use.
+    AvraCoinCrowdsale.setProvider(web3.currentProvider)
+    AvraCoin.setProvider(web3.currentProvider)
 
-  init: function() {
-    return App.initWeb3();
-  },
-
-  initWeb3: function() {
-    GustavoCoinCrowdsale.setProvider(web3.currentProvider);
-    GustavoCoin.setProvider(web3.currentProvider);
-    // Initialize web3 and set the provider to the testRPC.
+    // Get the initial account balance so it can be displayed.
     web3.eth.getAccounts(function (err, accs) {
       if (err != null) {
-        alert("There was an error fetching your accounts.");
-        return;
+        alert('There was an error fetching your accounts.')
+        return
       }
 
       if (accs.length == 0) {
-        alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
-        return;
+        alert('Couldn\'t get any accounts! Make sure your Ethereum client is configured correctly.')
+        return
       }
 
-      accounts = accs;
-      account = accounts[0];
-console.log(account)
-      self.refreshBalance();
-      self.setTotalSupply();
-    });
+      accounts = accs
+      account = accounts[0]
+      account1 = accounts[1]
+      console.log(account)
+
+      self.refreshBalance()
+    })
   },
+  getAvraCoinInstance: function (callback) {
+    var crowdsale = AvraCoinCrowdsale.deployed() //crowdsale is a promise
 
-  initContract: function() {
-    $.getJSON('GustavoCoinCrowdsale.json', function(data) {
-      // Get the necessary contract artifact file and instantiate it with truffle-contract.
-      var GustavoCoinCrowdsaleArtifact = data;
-       GustavoCoinCrowdsale = TruffleContract(GustavoCoinCrowdsaleArtifact);
-
-      // Set the provider for our contract.
-      GustavoCoinCrowdsale.setProvider(App.web3Provider);
-
-      // Use our contract to retieve and mark the adopted pets.
-    });
-
-    $.getJSON('GustavoCoin.json', function(data) {
-      // Get the necessary contract artifact file and instantiate it with truffle-contract.
-      var GustavoCoinArtifact = data;
-      GustavoCoin = TruffleContract(GustavoCoinArtifact);
-
-      // Set the provider for our contract.
-      GustavoCoin.setProvider(App.web3Provider);
-
-      // Use our contract to retieve and mark the adopted pets.
-    });
-
-    return App.getBalances();
-    return App.bindEvents();
-  },
-
-  bindEvents: function() {
-    $(document).on('click', '#transferButton', App.handleTransfer);
-  },
-
-  handleTransfer: function(event) {
-    event.preventDefault();
-
-    var amount = parseInt($('#TTTransferAmount').val());
-    var toAddress = $('#TTTransferAddress').val();
-
-    console.log('Transfer ' + amount + ' TT to ' + toAddress);
-
-    var GustavoCoinCrowdsaleInstanse;
-
-    web3.eth.getAccounts(function(error, accounts) {
-      if (error) {
-        console.log(error);
-      }
-
-      var account = accounts[0];
-
-      App.contracts.GustavoCoinCrowdsale.deployed().then(function(instance) {
-        GustavoCoinCrowdsaleInstanse = instance;
-
-        return GustavoCoinCrowdsaleInstanse.transfer(toAddress, amount, {from: account});
-      }).then(function(result) {
-        alert('Transfer Successful!');
-        return App.getBalances();
-      }).catch(function(err) {
-        console.log(err.message);
-      });
-    });
-  },
-
-  getBalances: function() {
-    console.log('Getting balances...');
-
-    var tokenAddress
-    var balance
-
-    web3.eth.getAccounts(function(error, accounts) {
-      if (error) {
-        console.log(error);
-      }
-
-      var account = accounts[0];
-      var crowdsale
-
-      GustavoCoinCrowdsale.deployed().then(function (instance) {
-        crowdsale = instance;
-      crowdsale.token.call().then(addr => {
-        console.log(addr)
-        gustavoCoinInstance = GustavoCoin.at(addr)
-        gustavoCoinInstance.balanceOf(account).then(balance => {
-          console.log(account)
-          console.log(balance)
-          console.log(balance.c[0])
-          $('#TTBalance').text(balance.c[0]);
+    crowdsale.then(crowdsaleContract => {//crowdsaleContract is Truffle contract with fields and methods
+      console.log('result address ' + crowdsaleContract.address)
+      console.log(crowdsaleContract)
+      crowdsaleContract.token() //call method token() of contract AvraCrowdsale
+        .then(tokenAddress => { // tokenAddress is a result of method token(). It creates new Avracoin and returns it's address
+          console.log('tokenAddress')
+          console.log(tokenAddress)
+          AvraCoin.at(tokenAddress)
+            .then(AvracoinContract => { //AvracoinContract is a Truffle contract with fields and methods
+              console.log('AvracoinContract')
+              console.log(AvracoinContract)
+              callback(AvracoinContract)//return instance of AvraCoin in callback
+            })
         })
-      } )
-
-
-
-
-        $('#TTBalance').text(balance);
-      })
 
     })
+  },
+
+  setStatus: function (message) {
+    var status = document.getElementById('status')
+    status.innerHTML = message
+  },
+
+  refreshBalance: function () {
+    var self = this
+    var crowdsale = AvraCoinCrowdsale.deployed() //crowdsale is a promise
+
+    function  getAvraCoinInstance  (callback) {
+      var crowdsale = AvraCoinCrowdsale.deployed() //crowdsale is a promise
+
+      crowdsale.then(crowdsaleContract => {//crowdsaleContract is Truffle contract with fields and methods
+        console.log('result address ' + crowdsaleContract.address)
+        console.log(crowdsaleContract)
+        crowdsaleContract.token() //call method token() of contract AvraCrowdsale
+          .then(tokenAddress => { // tokenAddress is a result of method token(). It creates new Avracoin and returns it's address
+            console.log('tokenAddress')
+            console.log(tokenAddress)
+            AvraCoin.at(tokenAddress)
+              .then(AvracoinContract => { //AvracoinContract is a Truffle contract with fields and methods
+                console.log('AvracoinContract')
+                console.log(AvracoinContract)
+                callback(AvracoinContract)//return instance of AvraCoin in callback
+              })
+          })
+
+      })}
+    function getAvraCoinBalance (account) {
+      getAvraCoinInstance(function (globalAvraCoinContract) { //call function which returns instance of AvraCoin
+        console.log('AvracoinContract!!!!!')
+        console.log(globalAvraCoinContract)
+        globalAvraCoinContract.balanceOf(account).then(balance => { //get balance of my account in AvraCoin
+          console.log(balance.toString(10))
+          // var temp = web3.utils.fromWei(balance);
+          // console.log(temp)
+
+          var balance_element = document.getElementById("balance");
+          balance_element.innerHTML = balance.toString(10);
+          balance.toString(10)
+        })
+      })
+    }
+
+    getAvraCoinBalance(account);
+    // AvraCoinCrowdsale.deployed()
+    //   .then(inst => {
+    //       console.log("This is instance")
+    //       console.log(inst)
+    //       inst.sendTransaction({ from: account, value: web3.toWei(5, "ether")});})
+   // self.refreshBalance();
+  },
+
+
+  sendCoin: function() {
+    var self = this;
+    var amount = parseInt(document.getElementById("amount").value);
+    AvraCoinCrowdsale.deployed()
+      .then(inst => {
+        console.log("This is instance")
+        console.log(inst)
+        inst.sendTransaction({ from: account, value: web3.toWei(amount, "ether")});
+        self.refreshBalance();
+      })
+    //  var receiver = document.getElementById("receiver").value;
+    //  function transferCoins(){
+    //   self.getAvraCoinInstance(function (globalAvraCoinContract){
+    //     globalAvraCoinContract.transferFrom({from: account}, {to: receiver}, amount);
+    //    // this.setStatus("Initiating transaction... (please wait)");
+    //  //   self.setStatus("Transaction complete!");
+    //     self.refreshBalance();})
+    //   }
+    //   transferCoins();
+
+  },
+  transferCoin: function() {
+    var self = this;
+    var amount = parseInt(document.getElementById("amount1").value);
+    var receiver = document.getElementById("receiver").value;
+    //  function transferCoins(){
+    //   self.getAvraCoinInstance(function (globalAvraCoinContract){
+    //     globalAvraCoinContract.transfer({from: account}, {to: receiver}, amount);
+    //    // this.setStatus("Initiating transaction... (please wait)");
+    //  //   self.setStatus("Transaction complete!");
+    //     self.refreshBalance();})
+    //   }
+    //   transferCoins();
+
   }
 
 };
 
-$(function() {
-  $(window).load(function() {
-    App.init();
-  });
-});
+window.addEventListener('load', function () {
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if (typeof web3 !== 'undefined') {
+    console.warn('Using web3 detected from external source. If you find that your accounts don\'t appear or you have 0 MetaCoin, ensure you\'ve configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask')
+    // Use Mist/MetaMask's provider
+    window.web3 = new Web3(web3.currentProvider)
+  } else {
+    console.warn('No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live, as it\'s inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask')
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:9545'))
+  }
+
+  App.start()
+})
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,20 +1,30 @@
 // var ConvertLib = artifacts.require("./ConvertLib.sol");
 // var MetaCoin = artifacts.require("./MetaCoin.sol");
-// var AvraToken = artifacts.require("./AvraToken.sol");
 //
 // module.exports = function(deployer) {
 //   deployer.deploy(ConvertLib);
 //   deployer.link(ConvertLib, MetaCoin);
 //   deployer.deploy(MetaCoin);
-//   deployer.deploy(AvraToken);
 // };
-const GustavoCoinCrowdsale = artifacts.require("./GustavoCoinCrowdsale.sol")
+
+// const AvraCoinCrowdsale = artifacts.require("./Crowdsale.sol")
+//
+// module.exports = function(deployer, network, accounts) {
+//   const startTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1 // one second in the future
+//   const endTime = startTime + (86400 * 20) // 20 days
+//   const rate = new web3.BigNumber(1000)
+//   const wallet = accounts[0]
+// console.log(startTime,endTime,wallet,rate);
+//   deployer.deploy(AvraCoinCrowdsale, startTime, endTime, rate, wallet)
+// };
+
+const Crowdsale = artifacts.require("./Crowdsale.sol")
 
 module.exports = function(deployer, network, accounts) {
   const startTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1 // one second in the future
   const endTime = startTime + (86400 * 20) // 20 days
-  const rate = new web3.BigNumber(1000)
+  const rate = 1000
   const wallet = accounts[0]
-
-  deployer.deploy(GustavoCoinCrowdsale, startTime, endTime, rate, wallet)
+  console.log(startTime,endTime,wallet,rate);
+  deployer.deploy(Crowdsale, startTime, endTime, rate, wallet)
 };
